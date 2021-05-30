b45208a3a3b3ffa673f2f1da15fa79c7e0cee827
= <10166308+eurubkov@users.noreply.github.com>
2021-05-29 23:40:47 -0700

    feat: Add Book Componet Add book component. Replace code for listing books in Books component with Book component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Book from "./Book";
 
 const Books = ({ books }) => {
   const booksByShelf = (books, category) => {
@@ -9,37 +10,9 @@ const Books = ({ books }) => {
         <div className="bookshelf-books">
           <ol className="books-grid">
             {books.map((book) => {
-              return (
-                <li key={book.id}>
-                  <div className="book">
-                    <div className="book-top">
-                      <div
-                        className="book-cover"
-                        style={{
-                          width: 128,
-                          height: 193,
-                          backgroundImage: `url(${book.imageLinks.thumbnail})`,
-                        }}
-                      />
-                      <div className="book-shelf-changer">
-                        <select>
-                          <option value="move" disabled>
-                            Move to...
-                          </option>
-                          <option value="currentlyReading">
-                            Currently Reading
-                          </option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.authors}</div>
-                  </div>
-                </li>
-              );
+              return <li key={book.id}>
+                  <Book book={book} />
+                </li>;
             })}
           </ol>
         </div>
